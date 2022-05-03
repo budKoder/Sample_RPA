7d72d27868e9c59c7e2c294bef952473e3372197
Ha0Lee <101060048+budKoder@users.noreply.github.com>
2022-05-03 10:08:50 +0900

    Refactor#04 make function
    make bot as function

---
+++ bot.py
@@ -0,0 +1,22 @@
+import time
+
+from web_bot import connect_to_url, move_tab_to_target, select_data_and_search, download_excel
+from file_control import is_download_finished, move_file
+from chrome_driver import generate_chrome, close_chrome
+from outlook import send_mail
+
+
+def run():
+    driver = generate_chrome()
+    # download excel file from web
+    connect_to_url(driver)
+    move_tab_to_target(driver)
+    select_data_and_search(driver)
+    download_excel(driver)
+    if is_download_finished():
+        time.sleep(3)
+        close_chrome(driver)
+
+    # move downloaded file in local environment
+    move_name = move_file()
+    send_mail(move_name)
